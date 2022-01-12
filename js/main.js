b7a96d16d4ccc98f45916faae761040c41187cad
Grady Robbins <gradyrobbins13@gmail.com>
2022-01-12 14:45:41 -0600

    initial js setup; listen to document upon readystatechange then launch app

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// console.log("hello world main.js");
+import ToDoList from "./todolist.js";
+import ToDoItem from "./todoitem.js";
+
+const toDoList = new toDoList();
+
+// launch app
+document.addEventListener("readystatechange", (event)=>{
+    
+})
