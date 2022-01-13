76b6fbd74a9be34a92eed3845883953afc228b10
Grady Robbins <gradyrobbins13@gmail.com>
2022-01-12 19:06:12 -0600

    javascript functions defined; TODO: clear persistent storage.  timestamp 1:14:27

--- js/main.js
+++ js/main.js
@@ -6,5 +6,74 @@ const toDoList = new toDoList();
 
 // launch app
 document.addEventListener("readystatechange", (event)=>{
-    
+    if(event.target.readyState ==="complete") {
+        initApp();
+    }
 })
+
+const initApp = () => {
+    //add listeners
+    //procedural
+    //load list object
+    //use webstorageAPI
+    //refresh the page
+    refreshThePage();
+}
+
+const refreshThePage = () =>{
+    clearListDisplay();
+    //renderList()
+    //clearItemEntryField()
+    //setFocusOnItemEntry()
+
+}
+
+const clearListDisplay = () =>{
+    const parentElement = document.getElementById("listItems");
+    const deleteContents(parent) = () =>{
+
+    }
+}
+
+const deleteContents = () =>{
+    let child = parentElement.lastElementChild;
+    while (child) {
+        parentElement.removeElement(child)
+        child = parentElement.lastElementChild;
+    }
+
+}
+
+const renderList = () =>{
+    const list = toDoList.getList();
+    list.foreach(item =>{
+        buildListItem(item)
+    })
+}
+
+const buildListItem = (item) =>{
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+    //addClickListenerToCheckbox(check)
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getItem();
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById("listItems");
+    container.appendChild(div);
+}
+
+const addClickListenerToCheckbox = (checkbox) => {
+    checkbox.addEventListener("click", (event) => {
+        toDoList.removeItemFromList(checkbox.id)
+        //todo: remove from persistent data
+        setTimeout(()=>{
+            refreshThePage();
+        },1000);
+    })
+}
