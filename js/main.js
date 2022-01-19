27b77aec79c526ce78f893a929f5cd39396ca8a2
Grady Robbins <gradyrobbins13@gmail.com>
2022-01-19 11:08:04 -0600

    debugging while rewatching video

--- js/main.js
+++ js/main.js
@@ -4,32 +4,32 @@ import ToDoItem from "./todoitem.js";
 const toDoList = new ToDoList();
 
 // launch app
-document.addEventListener("readystatechange", (event)=>{
-    if(event.target.readyState ==="complete") {
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
         initApp();
     }
 })
 
 const initApp = () => {
     //add listeners
-        const itemEntryForm = document.getElementById("itemEntryForm");
-        itemEntryForm.addEventListener("submit", (event)=>{
-            event.preventDefault();
-            processSubmission();
-        });
+    const itemEntryForm = document.getElementById("itemEntryForm");
+    itemEntryForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        processSubmission();
+    });
 
-        const clearItems = document.getElementById("clearItems");
-        clearItems.addEventListener("click" , (event)=>{
-            const list = toDoList.getList();
-            if(list.length){
-                const confirmed = confirm("are you sure?");
-                if(confirmed){
-                    toDoList.clearList();
-                    //TODO: update persistent storage
-                    refreshThePage();
-                }
+    const clearItems = document.getElementById("clearItems");
+    clearItems.addEventListener("click", (event) => {
+        const list = toDoList.getList();
+        if (list.length) {
+            const confirmed = confirm("are you sure?");
+            if (confirmed) {
+                toDoList.clearList();
+                //TODO: update persistent storage
+                refreshThePage();
             }
-        })
+        }
+    })
 
     //procedural
     loadListObject();
@@ -38,29 +38,29 @@ const initApp = () => {
     refreshThePage();
 }
 
-const loadListObject = () =>{
+const loadListObject = () => {
     const storedList = localStorage.getItem("MyToDoList")
-    if(typeof storedList !== "string") return;
+    if (typeof storedList !== "string") return;
     const parsedlist = JSON.parse(storedList);
-    parsedlist.forEach(itemObj =>{
+    parsedlist.forEach(itemObj => {
         const newToDoItem = createNewItem(itemObj._id, itemObj._item);
         toDoList.addItemToList(newToDoItem)
     })
 }
 
-const refreshThePage = () =>{
+const refreshThePage = () => {
     clearListDisplay();
     renderList()
     clearItemEntryField()
     setFocusOnItemEntry()
 }
 
-const clearListDisplay = () =>{
+const clearListDisplay = () => {
     const parentElement = document.getElementById("listItems");
     deleteContents(parentElement);
 }
 
-const deleteContents = (parentElement) =>{
+const deleteContents = (parentElement) => {
     let child = parentElement.lastElementChild;
     while (child) {
         parentElement.removeChild(child)
@@ -68,14 +68,14 @@ const deleteContents = (parentElement) =>{
     }
 }
 
-const renderList = () =>{
+const renderList = () => {
     const list = toDoList.getList();
-    list.forEach(item =>{
+    list.forEach(item => {
         buildListItem(item)
     })
 }
 
-const buildListItem = (item) =>{
+const buildListItem = (item) => {
     const div = document.createElement("div");
     div.className = "item";
     const check = document.createElement("input");
@@ -97,12 +97,11 @@ const addClickListenerToCheckbox = (checkbox) => {
         toDoList.removeItemFromList(checkbox.id);
         updatePersistentData(toDoList.getList());
         const removedText = getLabelText(checkbox.id);
-
         updateScreenReaderConfirmation(removedText, "removed from To Do List")
-        //todo: remove from persistent data
-        setTimeout(()=>{
+            //todo: remove from persistent data
+        setTimeout(() => {
             refreshThePage();
-        },1000);
+        }, 1000);
     })
 }
 
@@ -114,7 +113,7 @@ const updatePersistentData = (listArray) => {
     localStorage.setItem("MyToDoList", JSON.stringify(listArray))
 }
 
-const clearItemEntryField = () =>{
+const clearItemEntryField = () => {
     document.getElementById("newItem").value = "";
 }
 
@@ -124,7 +123,7 @@ const setFocusOnItemEntry = () => {
 
 const processSubmission = () => {
     const newEntryText = getNewEntry();
-    if(!newEntryText.length) return;
+    if (!newEntryText.length) return;
     const nextItemId = calcNextItemId();
     const toDoItem = createNewItem(nextItemId, newEntryText);
     toDoList.addItemToList(toDoItem);
@@ -134,20 +133,20 @@ const processSubmission = () => {
     refreshThePage();
 }
 
-const getNewEntry = () =>{
+const getNewEntry = () => {
     return document.getElementById("newItem").value.trim();
 }
 
-const calcNextItemId = () =>{
+const calcNextItemId = () => {
     let nextItemId = 1;
     const list = toDoList.getList();
-    if(list.length > 0 ){
-        nextItemId = list[list.length - 1].getId() +1;
+    if (list.length > 0) {
+        nextItemId = list[list.length - 1].getId() + 1;
     }
     return nextItemId;
 }
 
-const createNewItem = (itemId, itemText) =>{
+const createNewItem = (itemId, itemText) => {
     const toDo = new ToDoItem();
     toDo.setId(itemId);
     toDo.setItem(itemText);
@@ -155,5 +154,5 @@ const createNewItem = (itemId, itemText) =>{
 }
 
 const updateScreenReaderConfirmation = (newEntryText, actionVerb) => {
-    document.getElementById("confirmation").textContent = `${newEntryText} ${actionVerb}.`
+    return document.getElementById("confirmation").textContent = `${newEntryText} ${actionVerb}.`
 }
